67abd9ebf663aeff7008e85e3a56367ca793dabf
kelvo247 <B00119581@mytudublin.ie>
2025-02-20 01:13:49 +0000

    Updated login API with bcrypt fix

--- src/pages/register.js
+++ src/pages/register.js
@@ -53,7 +53,7 @@ const Register = () => {
             }}
         >
             <Typography variant="h4" component="h1" textAlign="center" gutterBottom>
-                Registe
+                Register
             </Typography>
             <TextField
                 fullWidth
